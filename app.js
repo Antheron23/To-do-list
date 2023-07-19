26e110ed6052309193e712eae81af16042fb3971
Ankit20 <128637838+Antheron23@users.noreply.github.com>
2023-07-19 21:26:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["buy food", "cook food", "eat food"];
+let workItems = [];
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
+
+app.get("/",function(req, res){
+     let today = new Date();
+     
+     let options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+     };
+
+     let day = today.toLocaleDateString("en-US", options);
+
+     res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+
+    let item = req.body.newItem;
+     if(req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+
+        }else{
+
+            items.push(item);
+            res.redirect("/");
+
+        }
+});
+
+app.get("/work", function(req, res){
+    res.render("list",{listTitle: "Work List", newListItems: workItems});
+});
+
+/*app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});*/
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+//nodemon app.js
